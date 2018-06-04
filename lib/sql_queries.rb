03475b65a9ffe603b773b568715f9be771f8b644
Felice Forby <feliceforby@gmail.com>
2018-06-04 22:54:31 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age AS female_bears FROM bears WHERE gender = 'F';"
+  "SELECT name, age FROM bears WHERE gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -7,19 +7,19 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age AS living_bears FROM bears WHERE alive = 1 ORDER BY age ASC;"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age AS oldest_bear FROM bears ORDER BY age DESC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age AS youngest_bear FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) AS most_prominent_color FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
+  "SELECT color AS most_prominent_color, COUNT(color) AS count FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
